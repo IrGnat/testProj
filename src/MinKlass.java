5aea3e9b5962e264b9aa6cea23681d3c0308d68d
Iryna Gnatenko <ira.gnatenko@gmail.com>
2020-09-29 15:17:38 +0200

    Initial commit

---
+++ src/MinKlass.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Iryna Gnatenko
+ * Date 9/29/2020
+ * Time 3:11 PM
+ * Project testProj
+ */
+public class MinKlass {
+
+    public static void main(String[] args) {
+        System.out.println("Hej");
+    }
+}
